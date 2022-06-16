92916bc1facac423eb9cbcb7ad3f3d45f25c8ac9
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-06-16 04:09:25 +0300

    Redesign your country badge

--- app/src/main/java/ru/alexander/worldmetrics/fragment/YourCountryFragment.kt
+++ app/src/main/java/ru/alexander/worldmetrics/fragment/YourCountryFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -11,19 +13,23 @@ import ru.alexander.worldmetrics.R
 import ru.alexander.worldmetrics.fragment.HomeScreenFragmentDirections.Companion.actionHomeScreenToCountryDetect
 import ru.alexander.worldmetrics.fragment.HomeScreenFragmentDirections.Companion.actionHomeScreenToCountryOverview
 import ru.alexander.worldmetrics.global.NavigationHelper.Companion.bindNavigation
-import ru.alexander.worldmetrics.model.CountriesData
+import ru.alexander.worldmetrics.model.CountriesData.Companion.getNameByCode
 import ru.alexander.worldmetrics.viewmodel.CurrentCountryViewModel
 
-class CountryBadgeFragment : Fragment(R.layout.country_badge_fragment) {
+class YourCountryFragment : Fragment(R.layout.your_country_fragment) {
+    private lateinit var locationMarker: ImageView
     private lateinit var countryInfo: TextView
     private lateinit var messageLabel: TextView
+    private lateinit var countryDetect: ImageButton
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val v = requireView()
         v.run {
+            locationMarker = findViewById(R.id.iv_location_marker)
             countryInfo = findViewById(R.id.tv_country_name)
             messageLabel = findViewById(R.id.tv_additional_message)
+            countryDetect = findViewById(R.id.ib_country_search_wizard)
         }
 
         val countryBadge: View = v.findViewById(R.id.cl_country_badge)
@@ -44,16 +50,22 @@ class CountryBadgeFragment : Fragment(R.layout.country_badge_fragment) {
 
     private fun setCountryCode(countryCode: String) {
         val countryName: String
-        messageLabel.run {
-            if (countryCode.isBlank()) {
-                countryName = getString(R.string.country_name_unknown)
+        if (countryCode.isBlank()) {
+            countryName = getString(R.string.country_name_unknown)
+            locationMarker.setImageResource(R.drawable.geolocation_unknown_marker)
+            messageLabel.run {
                 text = getString(R.string.consider_searching_your_country)
                 visibility = VISIBLE
-            } else {
-                countryName = CountriesData.getNameByCode(countryCode)
+            }
+            countryDetect.setImageResource(R.drawable.action_wizard)
+        } else {
+            countryName = getNameByCode(countryCode)
+            locationMarker.setImageResource(R.drawable.geolocation_marker)
+            messageLabel.run {
                 text = ""
                 visibility = GONE
             }
+            countryDetect.setImageResource(R.drawable.edit_icon)
         }
         countryInfo.text = countryName
     }
