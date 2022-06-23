50156afb02af3e3e6d51849ee5862c7ed2fe7424
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-06-23 21:32:03 +0300

    Calculate actual democracy index feature ranges, refactor CSV file

--- app/src/main/java/ru/alexander/worldmetrics/modules/democracy_index/service/impl/DemocracyIndexServiceImpl.kt
+++ app/src/main/java/ru/alexander/worldmetrics/modules/democracy_index/service/impl/DemocracyIndexServiceImpl.kt
@@ -11,15 +11,20 @@ class DemocracyIndexServiceImpl @Inject constructor(
     private companion object {
         const val MAX_YEAR = 2020
         const val COLUMN_COUNTRY_CODE = 0
-        const val COLUMN_YEAR = 2
-        const val COLUMN_INDEX_VALUE = 3
+        const val COLUMN_YEAR = 1
+        const val COLUMN_INDEX_VALUE = 2
+        const val COLUMN_ELECTORAL_PROCESS_AND_PLURALISM = 3
+        const val COLUMN_FUNCTIONING_OF_GOVERNMENT = 4
+        const val COLUMN_POLITICAL_PARTICIPATION = 5
+        const val COLUMN_POLITICAL_CULTURE = 6
+        const val COLUMN_CIVIL_LIBERTIES = 7
 
         val RANGE_VALUES = 10.8f to 98.1f
         val RANGE_ELECTORAL_PROCESS_AND_PLURALISM = 0.0f to 100.0f
-        val RANGE_FUNCTIONING_OF_GOVERNMENT = 0.0f to 100.0f
-        val RANGE_POLITICAL_PARTICIPATION = 0.0f to 100.0f
-        val RANGE_POLITICAL_CULTURE = 0.0f to 100.0f
-        val RANGE_CIVIL_LIBERTIES = 0.0f to 100.0f
+        val RANGE_FUNCTIONING_OF_GOVERNMENT = 0.0f to 96.4f
+        val RANGE_POLITICAL_PARTICIPATION = 5.6f to 100.0f
+        val RANGE_POLITICAL_CULTURE = 12.5f to 100.0f
+        val RANGE_CIVIL_LIBERTIES = 0.0f to 97.1f
     }
 
     lateinit var filePath: String
@@ -41,7 +46,7 @@ class DemocracyIndexServiceImpl @Inject constructor(
     override fun getLastYearData(country: String): DemocracyIndexValue {
         val rows = getDataForCountry(country)
         val row = rows.asSequence().let {
-            it.filter { row -> MAX_YEAR.equals(row[COLUMN_YEAR].toInt()) }.firstOrNull()
+            it.filter { row -> MAX_YEAR == row[COLUMN_YEAR].toInt() }.firstOrNull()
                 ?: it.filter { row -> (MAX_YEAR - 1) == row[COLUMN_YEAR].toInt() }
                     .firstOrNull()
         }
@@ -75,14 +80,14 @@ class DemocracyIndexServiceImpl @Inject constructor(
     override fun getCLRange() = RANGE_CIVIL_LIBERTIES
 
     private fun rowToIndexValue(row: List<String>): DemocracyIndexValue = DemocracyIndexValue(
-        row[0],
-        row[2].toInt(),
-        row[3].toFloat(),
-        row[4].toFloat(),
-        row[5].toFloat(),
-        row[6].toFloat(),
-        row[7].toFloat(),
-        row[8].toFloat(),
+        row[COLUMN_COUNTRY_CODE],
+        row[COLUMN_YEAR].toInt(),
+        row[COLUMN_INDEX_VALUE].toFloat(),
+        row[COLUMN_ELECTORAL_PROCESS_AND_PLURALISM].toFloat(),
+        row[COLUMN_FUNCTIONING_OF_GOVERNMENT].toFloat(),
+        row[COLUMN_POLITICAL_PARTICIPATION].toFloat(),
+        row[COLUMN_POLITICAL_CULTURE].toFloat(),
+        row[COLUMN_CIVIL_LIBERTIES].toFloat(),
     )
 
     private fun getDataForCountry(countryCode: String): List<List<String>> {
