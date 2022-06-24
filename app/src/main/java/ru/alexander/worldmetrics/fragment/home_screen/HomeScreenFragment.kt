fdc3846ee26729f43b836f94cce3c9c9a6bbc70f
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-06-24 22:53:48 +0300

    Create first working "About me" activity

--- app/src/main/java/ru/alexander/worldmetrics/fragment/home_screen/HomeScreenFragment.kt
+++ app/src/main/java/ru/alexander/worldmetrics/fragment/home_screen/HomeScreenFragment.kt
@@ -1,6 +1,30 @@
 package ru.alexander.worldmetrics.fragment.home_screen
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.alexander.worldmetrics.R
+import ru.alexander.worldmetrics.fragment.home_screen.HomeScreenFragmentDirections.Companion.actionHomeScreenToAboutMeActivity
+import ru.alexander.worldmetrics.global.NavigationHelper.Companion.navigateTo
 
-class HomeScreenFragment : Fragment(R.layout.home_screen)
+class HomeScreenFragment : Fragment(R.layout.home_screen) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_screen, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.about_me -> {
+            navigateTo(findNavController(), actionHomeScreenToAboutMeActivity())
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+}
