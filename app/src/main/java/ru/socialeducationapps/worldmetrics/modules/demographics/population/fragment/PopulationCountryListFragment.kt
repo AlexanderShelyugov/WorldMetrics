e44aa46f122321f254a81136bbadabf8a8466f0c
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-07-05 22:57:39 +0300

    Add country list to details navigation for population index

--- app/src/main/java/ru/socialeducationapps/worldmetrics/modules/demographics/population/fragment/PopulationCountryListFragment.kt
+++ app/src/main/java/ru/socialeducationapps/worldmetrics/modules/demographics/population/fragment/PopulationCountryListFragment.kt
@@ -1,10 +1,10 @@
 package ru.socialeducationapps.worldmetrics.modules.demographics.population.fragment
 
 import androidx.fragment.app.viewModels
-import androidx.navigation.NavDirections
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.Flow
 import ru.socialeducationapps.worldmetrics.fragment.CountriesListWithIndexFragment
+import ru.socialeducationapps.worldmetrics.modules.demographics.population.fragment.PopulationCountryListFragmentDirections.Companion.actionPopulationCountryListToPopulationDetail
 import ru.socialeducationapps.worldmetrics.modules.demographics.population.viewmodel.PopulationOverviewViewModel
 import ru.socialeducationapps.worldmetrics.modules.indexes.model.SimpleCountryValue
 
@@ -14,7 +14,6 @@ class PopulationCountryListFragment : CountriesListWithIndexFragment() {
 
     override fun getData(): Flow<List<SimpleCountryValue>> = model.lastYearData
     override fun getValueRange() = model.getTotalPopulationRange()
-    override fun getNavigationOnClick(countryCode: String): NavDirections? {
-        return super.getNavigationOnClick(countryCode)
-    }
+    override fun getNavigationOnClick(countryCode: String) =
+        actionPopulationCountryListToPopulationDetail(countryCode)
 }
