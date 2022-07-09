79b21114f96316a05a6da2f0b671bf68a14949d7
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-07-09 20:29:58 +0300

    Add transaction annotation for selection methods

--- app/src/main/java/ru/socialeducationapps/worldmetrics/modules/politics/press_freedom/room/dao/PressFreedomDao.kt
+++ app/src/main/java/ru/socialeducationapps/worldmetrics/modules/politics/press_freedom/room/dao/PressFreedomDao.kt
@@ -2,17 +2,21 @@ package ru.socialeducationapps.worldmetrics.modules.politics.press_freedom.room.
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import ru.socialeducationapps.worldmetrics.modules.indexes.model.SimpleCountryValue
 import ru.socialeducationapps.worldmetrics.modules.politics.press_freedom.room.entity.PressFreedomIndexValue
 
 @Dao
 interface PressFreedomDao {
+    @Transaction
     @Query("SELECT iso3CountryCode, pressFreedom AS value FROM pressfreedomindexvalue WHERE year = 2022")
     fun getLastYearData(): List<SimpleCountryValue>
 
+    @Transaction
     @Query("SELECT * FROM pressfreedomindexvalue WHERE iso3CountryCode = :country AND year = 2022")
     fun getLastYearData(country: String): PressFreedomIndexValue
 
+    @Transaction
     @Query("SELECT * FROM pressfreedomindexvalue WHERE iso3CountryCode = :country")
     fun getAllData(country: String): List<PressFreedomIndexValue>
 }
