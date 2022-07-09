8631ec17d9962cf643a5f6dcd0339365c8176184
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-07-09 22:43:09 +0300

    Refactor press freedom services

--- app/src/main/java/ru/socialeducationapps/worldmetrics/modules/politics/press_freedom/injections/PressFreedomModule.kt
+++ app/src/main/java/ru/socialeducationapps/worldmetrics/modules/politics/press_freedom/injections/PressFreedomModule.kt
@@ -5,16 +5,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import ru.socialeducationapps.worldmetrics.modules.politics.press_freedom.service.api.PressFreedomService
-import ru.socialeducationapps.worldmetrics.modules.politics.press_freedom.service.impl.PressFreedomCsvService
+import ru.socialeducationapps.worldmetrics.modules.politics.press_freedom.service.impl.PressFreedomRoomService
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class PressFreedomModule {
     @Provides
-    fun pressFreedomService(impl: PressFreedomCsvService): PressFreedomService = impl
-        .also { it.filePath = FILE_PATH }
-
-    companion object {
-        private const val FILE_PATH = "indexes/PressFreedomIndex.csv"
-    }
+    fun pressFreedomService(impl: PressFreedomRoomService): PressFreedomService = impl
 }
--- app/src/main/java/ru/socialeducationapps/worldmetrics/modules/politics/press_freedom/room/dao/PressFreedomDao.kt
+++ app/src/main/java/ru/socialeducationapps/worldmetrics/modules/politics/press_freedom/room/dao/PressFreedomDao.kt
@@ -9,14 +9,18 @@ import ru.socialeducationapps.worldmetrics.modules.politics.press_freedom.room.e
 @Dao
 interface PressFreedomDao {
     @Transaction
-    @Query("SELECT iso3CountryCode, pressFreedom AS value FROM pressfreedomindexvalue WHERE year = 2022")
+    @Query("SELECT iso3CountryCode, pressFreedom AS value FROM pressfreedomindexvalue WHERE year = $MAX_YEAR")
     fun getLastYearData(): List<SimpleCountryValue>
 
     @Transaction
-    @Query("SELECT * FROM pressfreedomindexvalue WHERE iso3CountryCode = :country AND year = 2022")
+    @Query("SELECT * FROM pressfreedomindexvalue WHERE iso3CountryCode = :country AND year = $MAX_YEAR")
     fun getLastYearData(country: String): PressFreedomIndexValue
 
     @Transaction
     @Query("SELECT * FROM pressfreedomindexvalue WHERE iso3CountryCode = :country")
     fun getAllData(country: String): List<PressFreedomIndexValue>
+
+    private companion object {
+        const val MAX_YEAR = 2022
+    }
 }
