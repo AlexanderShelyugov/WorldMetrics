bcc58705db826e4741bcc7f6637256967fd1867b
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2023-06-18 00:53:09 +0200

    Make country score more detailed - include all indexes and features

--- app/src/main/java/ru/socialeducationapps/worldmetrics/feature/country/score/viewmodel/CountryScoreViewModel.kt
+++ app/src/main/java/ru/socialeducationapps/worldmetrics/feature/country/score/viewmodel/CountryScoreViewModel.kt
@@ -10,8 +10,9 @@ import ru.socialeducationapps.worldmetrics.feature.index.economics.gdp.service.a
 import ru.socialeducationapps.worldmetrics.feature.index.politics.corruption_perceptions.service.api.CorruptionPerceptionsService
 import ru.socialeducationapps.worldmetrics.feature.index.politics.democracy_index.service.api.DemocracyIndexService
 import ru.socialeducationapps.worldmetrics.feature.index.politics.press_freedom.service.api.PressFreedomService
-import ru.socialeducationapps.worldmetrics.feature.indexes.all.model.AllIndexes
+import ru.socialeducationapps.worldmetrics.feature.indexes.all.model.AllIndexes.Companion.ALL_INDEXES
 import ru.socialeducationapps.worldmetrics.feature.indexes.common.model.FeatureMedianRange
+import ru.socialeducationapps.worldmetrics.feature.indexes.common.model.FeatureValue
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,59 +30,93 @@ class CountryScoreViewModel @Inject constructor(
     }
 
     private val scoreCalculators = mapOf(
-        R.string.index_name_population to this::calculatePopulationScore,
-        R.string.index_name_gdp to this::calculateGDPScore,
-        R.string.index_name_democracy to this::calculateDemocracyScore,
+        R.string.index_group_name_demographic to this::calculateDemographicScore,
+        R.string.index_group_name_economic to this::calculateEconomicalScore,
+        R.string.index_group_name_political to this::calculatePoliticalScore,
     )
 
-    private val ranges = runBlocking {
-        mapOf(
-            R.string.index_name_democracy to democracyIndexService.getValueRange(),
-            R.string.index_name_corruption_perceptions to corruptionPerceptionsService.getValueRange(),
-            R.string.index_name_press_freedom to pressFreedomService.getValueRange(),
-            R.string.index_name_gdp to gdpService.getValueMlnUsdRange()
-        )
-    }
-
     fun getCountryScores(country: String): Map<Int, Int> {
-        return AllIndexes.ALL_INDEXES.flatMap { it.indexes }.map { index ->
-            val scoreCalculator = scoreCalculators[index.name]
-            scoreCalculator?.let { index.name to it(country) }
-        }
-            .filterNotNull()
+        return ALL_INDEXES.map { it.name }
+            .mapNotNull { indexGroupId ->
+                val scoreCalculator = scoreCalculators[indexGroupId]
+                scoreCalculator?.let { indexGroupId to it(country) }
+            }
             .toMap()
     }
 
-    private fun calculatePopulationScore(country: String): Int {
-        return calculateScore(
-            { runBlocking(dispatchers.io) { populationService.getLastYearData(country) }.populationDensity },
-            { runBlocking(dispatchers.io) { populationService.getPopulationDensityRange() } }
-        )
+    private fun calculateDemographicScore(country: String): Int {
+        val valuesAndRanges = runBlocking(dispatchers.io) {
+            val lastYearData = populationService.getLastYearData(country)
+            listOf(
+                lastYearData.populationTotal to populationService.getTotalPopulationRange(),
+                lastYearData.populationMale to populationService.getMalePopulationRange(),
+                lastYearData.populationFemale to populationService.getFemalePopulationRange(),
+                lastYearData.populationDensity to populationService.getPopulationDensityRange(),
+            )
+        }
+
+        return calculateScore(valuesAndRanges)
     }
 
-    private fun calculateGDPScore(country: String): Int {
-        return calculateScore(
-            { runBlocking(dispatchers.io) { gdpService.getLastYearData(country) }.valueUsdCap },
-            { gdpService.getValueUsdPerCapitaRange() }
-        )
+    private fun calculateEconomicalScore(country: String): Int {
+        val valuesAndRanges = runBlocking(dispatchers.io) {
+            val lastYearData = gdpService.getLastYearData(country)
+            listOf(
+                lastYearData.valueMlnUsd to gdpService.getValueMlnUsdRange(),
+                lastYearData.valueUsdCap to gdpService.getValueUsdPerCapitaRange(),
+            )
+        }
+
+        return calculateScore(valuesAndRanges)
     }
 
-    private fun calculateDemocracyScore(country: String): Int {
-        return calculateScore(
-            { runBlocking(dispatchers.io) { democracyIndexService.getLastYearData(country) }.democracyIndex },
-            { democracyIndexService.getValueRange() }
-        )
+    private fun calculatePoliticalScore(country: String): Int {
+        val valuesAndRanges = runBlocking(dispatchers.io) {
+            val demLastYearData = democracyIndexService.getLastYearData(country)
+            val demValues = listOf(
+                demLastYearData.democracyIndex to democracyIndexService.getValueRange(),
+                demLastYearData.civilLiberties to democracyIndexService.getCLRange(),
+                demLastYearData.politicalCulture to democracyIndexService.getPCRange(),
+                demLastYearData.electoralProcessAndPluralism to democracyIndexService.getEPAPRange(),
+                demLastYearData.functioningOfGovernment to democracyIndexService.getFOGRange(),
+                demLastYearData.politicalParticipation to democracyIndexService.getPPRange(),
+            )
+
+            val corrupLYD = corruptionPerceptionsService.getLastYearData(country)
+            val corrupValues = listOf(
+                corrupLYD.value to corruptionPerceptionsService.getValueRange()
+            )
+
+            val pressLYD = pressFreedomService.getLastYearData(country)
+            val pressValues = listOf(
+                pressLYD.score to pressFreedomService.getValueRange(),
+                pressLYD.politicalContext to pressFreedomService.getPCRange(),
+                pressLYD.economicContext to pressFreedomService.getECRange(),
+                pressLYD.legalContext to pressFreedomService.getLCRange(),
+                pressLYD.socialContext to pressFreedomService.getSCRange(),
+                pressLYD.safety to pressFreedomService.getSRange(),
+            )
+
+            listOf(demValues, corrupValues, pressValues).flatten()
+        }
+
+        return calculateScore(valuesAndRanges)
     }
 
     private fun calculateScore(
-        indexExtractor: () -> Float, rangeExtractor: () -> FeatureMedianRange
+        valuesAndRanges: List<Pair<FeatureValue, FeatureMedianRange>>
     ): Int {
-        val range = rangeExtractor.invoke()
-        return scoreWithinRange(indexExtractor.invoke(), range.first, range.third)
+        val averageRatio = valuesAndRanges
+            .filter { !it.first.isNaN() && it.first >= 0 }
+            .map { ratio(it.first, it.second.first, it.second.third) }
+            .average()
+            .toFloat()
+            .takeIf { it.isFinite() } ?: 0f
+        return (averageRatio * SCORE_TOP).toInt()
     }
 
-    private fun scoreWithinRange(x: Float, min: Float, max: Float): Int {
-        val ratio = (x - min) / (max - min) // 0..1
-        return (ratio * SCORE_TOP).toInt()
+    private fun ratio(x: Float, min: Float, max: Float): Float {
+        require(x in min..max)
+        return (x - min) / (max - min) // 0..1
     }
 }
